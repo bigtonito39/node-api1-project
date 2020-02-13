38fec874e96bf2ea3269280bc5ff86da3b5cad53
Reinoso <jreinoso@amazon.com>
2020-02-13 15:32:45 -0500

    project started, API is loaded, now i just need to start making the endpoints

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+const express = require("express")
+
+const server = express()
+const port = 3000
+
+//middleware that allows express to parse JSON request bodies
+server.use(express.json())
+
+server.get("/", (req, res) => {
+    res.json({message:"Testing my server, i will remove this after im done with project"})
+})
+
+
+// start the server on localhost at port 3000
+server.listen(port, ()=> {
+    console.log(`server started at http://localhost:${port}`)
+})
+
